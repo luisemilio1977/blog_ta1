e649c3ff287b705dc4f2cf0e995010dc8e6da84f
luisemilio1977 <lmendez@fagro.edu.uy>
2023-07-10 18:40:05 -0300

    mejorando vistas

---
+++ resources/views/header.blade.php
@@ -0,0 +1,10 @@
+<CENTER>
+<TABLE FRAME="hsides" RULES="none">
+	<TR>
+		<TD><a href="/posts/create">Crear Post</a></TD> <TD><a href="/posts/index">Listar Posts</a></TD> <TD>C</TD>
+	</TR>
+	<TR>
+		<TD>...</TD> <TD>...</TD> <TD>...</TD>
+	</TR>
+</TABLE>
+</CENTER>
--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,13 +1,14 @@
 <!DOCTYPE html> 
 <html> 
 <head> 
-<title>Blog sederhana</title> 
+<title></title> 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> 
 </head> 
-<body> 
-<div class="container"> 
-@yield('content') 
-</div> 
+<body>
+    @include("header")
+     <div class="container"> 
+    @yield('content') 
+    </div> 
 </body> 
 </html> 
   
